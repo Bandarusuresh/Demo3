47f2544b836ec461016297c31b4bda9f6d0bb4a1
Fname Lname <username@Fnames-MacBook-Pro.local>
2018-03-08 17:52:29 +0530

    Upload

--- Demo3/ViewController.m
+++ Demo3/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"succesfully completed github learning");
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
